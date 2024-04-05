c3cc5b014b5c5d7be3fdcc868fbba349df70c30b
Dmitrii Kushchenko <dikushch@gmail.com>
2024-04-05 19:08:33 +0300

    feat: add partialsingrguments function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -215,8 +215,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  function f(...args) {
+    return fn(...args1, ...args);
+  }
+  return f;
 }
 
 /**
