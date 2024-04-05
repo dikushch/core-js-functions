8bd6f9da71f226dfc47cddf709442c39029c56fd
Dmitrii Kushchenko <dikushch@gmail.com>
2024-04-05 10:44:11 +0300

    feat: add getFunctionBody function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,12 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  let body = '';
+  if (func) {
+    body = func.toString();
+  }
+  return body;
 }
 
 /**
